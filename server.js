3ca3bce04d476ea84e5d68304d4a89f0faf5727d
Rupesh sahu <rupeshsahu969@gmail.com>
2026-02-03 21:44:25 +0530

    update

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 app.get("/proxy", async (req, res) => {
   try {
-    const response = await axios.get("https://script.google.com/a/macros/botivate.in/s/AKfycbxjYYdBHyeK1n65Er6c76ymzKvBvZr8ixit2_OUTRA/dev"); 
+    const response = await axios.get("https://script.google.com/a/macros/botivate.in/s/AKfycbxjYYdBHyeK1n65Er6c76ymzKvBvZr8ixit2_OUTRA/dev");
     res.json(response.data);
   } catch (error) {
     res.status(500).json({ error: error.message });
